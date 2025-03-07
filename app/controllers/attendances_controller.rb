6a449b2d9713ea7bbd38c886afe9347383e928b8
KatoKoshi0209 <doradorayuji@gmail.com>
2025-03-07 23:09:00 +0000

    勤怠見た目の実装

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,19 @@
 class AttendancesController < ApplicationController
-  def new
+  def create
+  end
+
+  def update
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  
+  def attendance_params
+    params.permit(:user_id, :attendance_time, :break_start_time, :break_end_time, :leave_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users # devise を使用する際に URL として users を含む
-  resources :attendances, only: [:new, :index, :show]
+  resources :attendances, only: [:new, :create, :update, :index, :show]
   devise_scope :user do
     root to: "devise/sessions#new"
   end  
