ad70ffaaec83989184fbecc1d5bd5e7b2426ac9c
KatoKoshi0209 <doradorayuji@gmail.com>
2025-03-08 13:38:53 +0000

    ルーティング変更、編集画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users # devise を使用する際に URL として users を含む
   resources :attendances, only: [:new, :create, :update, :index, :show]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   devise_scope :user do
     root to: "devise/sessions#new"
   end  
