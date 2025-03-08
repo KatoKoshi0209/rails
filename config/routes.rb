38a27658d0d24d871c4943306c5fa24a4a24e45e
KatoKoshi0209 <doradorayuji@gmail.com>
2025-03-08 09:04:33 +0000

    画像機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users # devise を使用する際に URL として users を含む
   resources :attendances, only: [:new, :create, :update, :index, :show]
+  resources :users, only: [:show, :edit]
   devise_scope :user do
     root to: "devise/sessions#new"
   end  
