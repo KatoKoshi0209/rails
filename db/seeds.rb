8d3491c7b1b0434b50d2905d60127dd4f5582a6d
KatoKoshi0209 <doradorayuji@gmail.com>
2025-03-09 23:44:45 +0000

    ダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,22 @@ User.create!(name: "加藤航詩",
             password: "k89310141",
             password_confirmation: "k89310141",
             administrator: false)
+
+require 'date'
+
+user_id = 2
+start_date = Date.today - 90  # 90日前から
+
+attendances = (0..89).map do |i|
+  date = start_date + i
+  {
+    attendance_time: Time.zone.parse("#{date} 09:00:00"),
+    leave_time: Time.zone.parse("#{date} 18:00:00"),
+    break_start_time: Time.zone.parse("#{date} 12:00:00"),
+    break_end_time: Time.zone.parse("#{date} 13:00:00"),
+    user_id: user_id
+  }
+end
+
+Attendance.create!(attendances)
+            
